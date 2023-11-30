3088f7ac3ba1de1dd51a2eb328240455659d51d6
martaguillemolmos <martetaguillem@gmail.com>
2023-11-30 22:54:53 +0100

    Add: Create the content of the Room model.

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -4,8 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+
 
 class Room extends Model
 {
     use HasFactory;
+
+    //Relación uno a muchos a la tabla a Message y Member
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Message::class);
+        return $this->hasMany(Member::class);
+
+    }
+
+    //Relacion muchos a uno a la tabla Game
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Game::class);
+    }
+
+    //Relación muchos a muchos con la tabla User
+    public function roles(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class);
+    }
 }
