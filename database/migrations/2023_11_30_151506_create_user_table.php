c1c57078f613caabac82ef2eaf5d4ce3a5479fb9
Gastón <gastonvalentiniruiz@gmail.com>
2023-11-30 17:27:34 +0100

    User migration configured.

--- database/migrations/2023_11_30_151506_create_user_table.php
+++ database/migrations/2023_11_30_151506_create_user_table.php
@@ -13,15 +13,16 @@ return new class extends Migration
     {
         Schema::create('user', function (Blueprint $table) {
             $table->id();
-            $table->string("name");
-            $table->string("surname");
-            $table->string("nickname");
-            $table->string("email");
+            $table->string("name", 50);
+            $table->string("surname", 50);
+            $table->string("nickname", 50)->unique();
+            $table->string("email", 50)->unique();
             $table->string("password");
             $table->enum('role', ["user", "admin"]);
-            $table->string("image");
-            $table->timestamps();
-            $table->string("is_active");
+            $table->string("image", 255);
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('update_at')->useCurrent()->useCurrentOnUpdate();
+            $table->boolean("is_active")->default(true);
         });
     }
 
