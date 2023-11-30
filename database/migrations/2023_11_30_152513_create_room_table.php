9526be0f9b1587224022c1600e4787861f92a478
Gastón <gastonvalentiniruiz@gmail.com>
2023-11-30 16:33:09 +0100

    Add: Room migration template added.

---
+++ database/migrations/2023_11_30_152513_create_room_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('room', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('user_id')->references('id')->on('user');
+            $table->foreign('game_id')->references('id')->on('game');
+            $table->string('name');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('room');
+    }
+};
