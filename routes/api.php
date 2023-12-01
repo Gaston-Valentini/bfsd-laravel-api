6fe6abe46db7ac53b90d2af8a97c038db97b5bfe
martaguillemolmos <martetaguillem@gmail.com>
2023-12-01 09:50:36 +0100

    Add:Healthcheck Route.

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Healthcheck Route
+Route::get('/', function (Request $request) {
+    return response()->json(
+        [
+            "success" => true,
+            "message" => "Healthcheck ok"
+        ],
+        Response::HTTP_OK //code status 200
+    );
 });
+
