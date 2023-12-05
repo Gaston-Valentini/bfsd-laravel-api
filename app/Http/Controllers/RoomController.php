952ad38bd8784ed7ad6bd44951dc04f13022215d
martaguillemolmos <martetaguillem@gmail.com>
2023-12-05 12:48:22 +0100

    Fix: Problems with merge.

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -30,15 +30,6 @@ class RoomController extends Controller
         $roomsActive = Room::where("is_active", true)
                         ->get();
 
-        if(empty($roomsActive)==false){
-            return response()->json(
-                [
-                    "success" => true,
-                    "message" => "No hay",
-                ],
-                Response::HTTP_OK
-            );
-        } else {
             return response()->json(
                 [
                     "success" => true,
@@ -47,7 +38,6 @@ class RoomController extends Controller
                 ],
                 Response::HTTP_OK
             );
-            }
         }
 
 
