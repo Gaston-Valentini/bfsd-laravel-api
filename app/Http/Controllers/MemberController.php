471f777666ba7794cdf4e943774c0b5361ca1cc2
Toni <antonioinsa@tutanota.com>
2023-12-06 12:12:52 +0100

    Add:GetMemberById in memberController.

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -68,5 +68,31 @@ class MemberController extends Controller
         }
     }
 
+    public function getMemberById($id)
+    {
+        try {
+            $member = Member::with(["user", "room"])->find($id)->get();
+
+            return response()->json(
+                [
+                    "success" => true,
+                    "message" => "Get member",
+                    "data" => $member
+                ],
+                Response::HTTP_OK
+            );
+        } catch (\Throwable $th) {
+            Log::error($th->getMessage());
+
+            return response()->json(
+                [
+                    "success" => false,
+                    "message" => "Error getting member"
+                ],
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+    }
+
     
 }
