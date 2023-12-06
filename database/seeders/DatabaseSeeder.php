4e41e7af091f682e74731c5c6754353f5597a749
Gastón <gastonvalentiniruiz@gmail.com>
2023-12-06 11:18:04 +0100

    Add: Seeders added.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,21 +2,55 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
-class DatabaseSeeder extends Seeder
+class UserSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('users')->insert(
+            [
+                'name' => Str::random(30),
+                "surname" => Str::random(30),
+                "nickname" => Str::random(30),
+                'email' => 'user@user.com',
+                'password' => Hash::make('password'),
+            ]
+        );
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        DB::table('users')->insert(
+            [
+                'name' => Str::random(2),
+                "surname" => Str::random(2),
+                "nickname" => Str::random(2),
+                'email' => 'admin@admin.com',
+                'password' => Hash::make('password'),
+                'role' => "admin"
+            ]
+        );
+
+        DB::table('games')->insert(
+            [
+                'name' => Str::random(10),
+                'description' => Str::random(10),
+            ]
+        );
+
+        DB::table('rooms')->insert(
+            [
+                'name' => Str::random(5),
+            ]
+        );
+
+        DB::table('message')->insert(
+            [
+                'message' => Str::random(100),
+            ]
+        );
     }
 }
+
