dc1d461251164415f9347344927973a07932373b
Toni <antonioinsa@tutanota.com>
2023-12-06 11:09:28 +0100

    Add:relations user/member

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,5 +48,9 @@ class User extends Model
     {
         return $this->hasMany(Message::class);
     }
-    
+
+    public function members(): HasMany
+    {
+        return $this->hasMany(Member::class);
+    }
 }
